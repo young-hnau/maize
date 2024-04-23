527a4101169c74516648910b3ab0f3079e11ab96
Ritter99 <isaiahhritter0@gmail.com>
2024-04-23 13:41:19 -0400

    Refactor: Removing dead code

--- lib/Pages/Home/Page/Drawer/drawer_cubit.dart
+++ lib/Pages/Home/Page/Drawer/drawer_cubit.dart
@@ -1,7 +1,6 @@
 import 'package:equatable/equatable.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:mealie_mobile/Pages/Home/Page/Drawer/CreateOverlay/create_overlay_widget.dart';
 import 'package:mealie_mobile/app/app_bloc.dart';
 
 part 'drawer_state.dart';
@@ -15,16 +14,4 @@ class DrawerCubit extends Cubit<DrawerState> {
   final AppBloc appBloc;
 
   Future<void> _initialize() async {}
-
-  void removeCreateOverlay() {
-    state.createOverlay!.remove();
-    emit(state.copyWith(status: state.status, createOverlay: null));
-  }
-
-  void addCreateOverlay(BuildContext context) {
-    OverlayEntry overlayEntry = CreateOverlay(drawerCubit: this);
-    Overlay.of(context).insert(overlayEntry);
-
-    emit(state.copyWith(status: state.status, createOverlay: overlayEntry));
-  }
 }
