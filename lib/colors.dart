5a96fdfeebea2d868b713cbef0c202c952ce59ab
YoungHnau <isaiahhritter0@gmail.com>
2024-06-04 09:21:16 -0400

    Update: Colors better match Mealie

--- lib/colors.dart
+++ lib/colors.dart
@@ -1,13 +1,13 @@
 import 'package:flutter/material.dart';
 
 class MealieColors {
-  static const orange = Color.fromARGB(255, 218, 136, 54);
-  static const green = Color.fromARGB(255, 91, 156, 78);
+  static const orange = Color.fromARGB(255, 225, 120, 33);
+  static const green = Color.fromARGB(255, 59, 150, 62);
   static const red = Color.fromARGB(255, 140, 47, 58);
-  static const brightRed = Color.fromARGB(255, 235, 72, 69);
+  static const brightRed = Color.fromARGB(255, 236, 73, 70);
   static const darkRed = Color.fromARGB(255, 112, 25, 39);
-  static const peach = Color.fromARGB(255, 239, 132, 139);
-  static const lightBlue = Color.fromARGB(255, 76, 154, 176);
+  static const peach = Color.fromARGB(255, 236, 73, 70);
+  static const lightBlue = Color.fromARGB(255, 67, 152, 175);
   static const blue = Color.fromARGB(255, 24, 107, 204);
   static const darkBlue = Color.fromARGB(255, 0, 111, 142);
 }
